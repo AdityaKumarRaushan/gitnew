f55acb430ea20f078f7aa9ff22ae4df07b3f042a
Aditya <aditya21bcy42@iiitkottayam.ac.in>
2022-12-08 00:06:35 +0530

    second commit in py.cpp

--- py.cpp
+++ py.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main(){
     cout << "hello , my name is aditya" << endl;
-    cout << "I have written this code for pusing in the github repository online" << endl;
+    // cout << "I have written this code for pusing in the github repository online" << endl;
+    cout << "we have enjoied it pushing on github" << endl;
+
 }
