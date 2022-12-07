540acc6c02169250b05d24d1e59bb91df0a2f69f
Aditya <aditya21bcy42@iiitkottayam.ac.in>
2022-12-07 23:58:33 +0530

    cpp code commited

---
+++ py.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "hello , my name is aditya" << endl;
+    cout << "I have written this code for pusing in the github repository online" << endl;
+}
